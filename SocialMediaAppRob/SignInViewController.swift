96336fc1d13de63ba9038268d6727631bba2c4ec
Roberto E. Cruz Quesada <rcquesada@gmail.com>
2016-12-04 01:34:51 -0400

    Facebook authentication complete

--- SocialMediaAppRob/SignInViewController.swift
+++ SocialMediaAppRob/SignInViewController.swift
@@ -7,18 +7,54 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
+
 
 class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("ROB: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("ROB: Successfully authenticated with Firebase")
+            }
+        })
+    }
+
+    @IBAction func facebookButtonPressed(_ sender: Any) {
+    
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("ROB: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("ROB: User cancelled Facebook authentication.")
+            } else {
+                print("ROB: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    //        Actual Facebook login button...
+    //        let loginButton = FBSDKLoginButton.init()
+    //        loginButton.center = self.view.center
+    //        self.view.addSubview(loginButton)
 
 
 }
