f1f70291515ebcf9b7c97a63b0421f32f5eacc61
Roberto E. Cruz Quesada <rcquesada@gmail.com>
2016-12-04 17:24:49 -0400

    Completed email authentication with Firebase

--- SocialMediaAppRob/SignInViewController.swift
+++ SocialMediaAppRob/SignInViewController.swift
@@ -14,6 +14,9 @@ import Firebase
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet var emailTextField: ABetterTextField!
+    @IBOutlet var passwordTextField: ABetterTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -41,7 +44,7 @@ class SignInViewController: UIViewController {
             if error != nil {
                 print("ROB: Unable to authenticate with Facebook - \(error)")
             } else if result?.isCancelled == true {
-                print("ROB: User cancelled Facebook authentication.")
+                print("ROB: User cancelled Facebook authentication")
             } else {
                 print("ROB: Successfully authenticated with Facebook")
                 let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
@@ -55,6 +58,24 @@ class SignInViewController: UIViewController {
     //        let loginButton = FBSDKLoginButton.init()
     //        loginButton.center = self.view.center
     //        self.view.addSubview(loginButton)
+
+    @IBAction func signInButtonPressed(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("ROB: Email user authenticated with Firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("ROB: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("ROB: Successfully authenticated with Firebase")
+                        }
+                })
+                }
+            })
+        }
+    }
 
 
 }
